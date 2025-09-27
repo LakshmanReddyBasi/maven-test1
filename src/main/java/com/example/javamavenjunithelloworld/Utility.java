e878debe1e6936b2f2c27bf4f321f30bf57fc2b6
lakshman <22pa1a0514@vishnu.edu.in>
2025-09-27 18:48:59 +0530

    LAKSHMANB0001 - Wrote code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,9 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {    
+    //Find the area of the circle
+    public static double lakshman(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+}
